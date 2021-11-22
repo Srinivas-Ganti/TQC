9c857dd185d56ddab905f72042f4814523ee1bbc
Srinivas Ganti RGLab <srinivas.ganti@ramglobal.com>
2021-11-23 00:42:35 +0100

    add partial qc param initiation

--- Model/TheaQC.py
+++ Model/TheaQC.py
@@ -8,22 +8,57 @@ sys.path.append(baseDir)
 from Model.experiment import *
 from MenloLoader import MenloLoader
 
+
 class TheaQC(Experiment):
     
     def __init__(self, loop = None, configFile = None):
         super().__init__(loop, configFile)
-        
+        self.initialise()
 
     def initialise(self):
 
         if self.configLoaded:
             self.initAttribs()
+            self.initResources()
             self.mLoader = MenloLoader([])          # fileLoader object
+            self.TdsWin = self.config['TScan']['window']
+
+
+    def initResources(self):
+
+        """
+            Initialise class attributes for QC application with default values.
+        """
+      
+        self.plotDataContainer = {'currentAveragePulseFft': None, 'livePulseFft': None}       # Dictionary for plot items
+        self.startOk = False                                                                  # Flag for TDS pulse start time validation
+        self.endOk = False                                                                    # Flag for TDS pulse end time validation
+        self.avgsOk = False                                                                   # Flag for required pulse averages validation
+        self.waferIdOk = False                                                                # Flag for wafer ID validation
+        self.sensorIdOk = False                                                               # Flag for sensor ID validation
+        self.repeatsOk = False                                                                # Flag for timelapse repeats validation
+        self.intervalOK = False                                                               # Flag for timelapse duration validation
+        self.tlapseSaveDir = None                                                             # Path for saving timelapse data
+        self.classification = None                                                            # Result of TDS pulse classification.
+        self.avgTask = None                                                                   # averaging task (manual - button event)
+        self.qcAvgTask = None                                                                 # QC averaging task (automatic - cartridge sensing)
+        self.timelapseTask = None                                                             # timelapse task (manual)
+        self.pulsePeaks = None                                                                # result of peak finding on TDS pulse for classification
+        self.qcParams = {}                                                                    # empty dictionary to hold qc parameters
+        self.qcComplete = False                                                               # Flag to track qc task completeion
+        self.qcResult = None                                                                  # QC result 
+        self.qcRunNum = 0                                                                     # tracking inspected sensors
+        self.qcRunning = False                                                                # Flag for QC running status
+        self.previousClassification = None                                                    # previous classification result 
+        self.qcResultsList = []                                                               # empty list to hold accumulated QC data on multiple sensors 
 
 
     def initAttribs(self):
 
-        self.plotDataContainer = {'currentAveragePulseFft': None, 'livePulseFft': None}
+        """
+            Initialise class attributes for QC, timelapse application with default values.
+        """
+
         self.pulseAmp = None                       # Received pulse data              
         self.avgPulseAmp = None                    # Averaged result
         self.timeAxis = None                       # time data 
@@ -73,6 +108,27 @@ class TheaQC(Experiment):
         self.timeAxis = self.mLoader.getTDS(src, [])[0]['time']
         
 
+    def loadQcConfig(self):
+        
+        """
+            Load QC paramters for comparitive error checking against standard reference from config file.
+        """
+
+        self.classificationDistance = self.config['Classification']['distance']
+        self.classificationProminence = self.config['Classification']['prominence']
+        self.classificationThreshold = self.config['Classification']['threshold']
+        self.classificationWidth = self.config['Classification']['width']
+        self.qcParams ={'fLB':self.config['QC']['lowerFreqBound'],
+                        'fUB':self.config['QC']['upperFreqBound'],
+                        'errTh':self.config['QC']['allowedErrordB'],
+                        'nErrV':self.config['QC']['maxViolations']}
+        qcSaveDir = self.config['QC']['qcSaveDir']
+        if qcSaveDir is None or not os.path.isdir(qcSaveDir):
+            self.qcSaveDir = os.path.join(baseDir, "qcData")
+            print(f"Setting default directory for qcData at: {self.qcSaveDir}")
+        else:
+            self.qcSaveDir = qcSaveDir
+            print(f"QC data will be saved in: {self.qcSaveDir}")
 
 
 #*********************************************************************************************************************
