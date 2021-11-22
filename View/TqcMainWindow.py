fc45021fef94d22490fd6d4c389d8cc67b6322a4
Srinivas Ganti RGLab <srinivas.ganti@ramglobal.com>
2021-11-22 21:23:11 +0100

    update UI, migrate old attribs

--- View/TqcMainWindow.py
+++ View/TqcMainWindow.py
@@ -10,6 +10,7 @@ uiDir = os.path.join(viewDir, "Designer")
 sys.path.append(baseDir)
 sys.path.append(modelDir)
 sys.path.append(viewDir)
+sys.path.append(os.path.join(viewDir, "Icons"))
 sys.path.append(uiDir)
 
 from Model.TheaQC import *
@@ -25,10 +26,53 @@ class TqcMainWindow(QWidget):
         self.experiment = experiment
         self.initUI()
 
+
+    def initAttribs(self):
+
+        """
+            Initialise class attributes for QC, timelapse application with default values.
+        """
+
+        self.height = 900
+        self.width = 720
+        self.left = 10
+        self.top = 40      
+        self.setGeometry(self.left, self.top, self.width, self.height)   
+
+        self.livePlot.setXRange(0, 5.3, padding = 0)
+        self.livePlot.setYRange(-250, -90, padding = 0)
+        self.colorLivePulse = (66,155,184, 145)
+        self.colorlivePulseBackground = (66,155,184,145)
+        self.colorAvgPulse = (200, 200, 200, 160)
+        self.colorstdRef = (160,150,27,140)
+        self.colorPass = (125,245,0,190)
+        self.colorFail = (255,0,0,190)
+        self.livePlotLineWidth = 1
+        self.averagePlotLineWidth = 1.5
+    
+        self.lEditTdsStart.setAlignment(Qt.AlignCenter) 
+        self.lEditTdsEnd.setAlignment(Qt.AlignCenter) 
+        self.lEditTdsAvgs.setAlignment(Qt.AlignCenter) 
+        self.lEditInterval.setAlignment(Qt.AlignCenter) 
+        self.lEditRepeatNum.setAlignment(Qt.AlignCenter) 
+        self.lEditWaferId.setAlignment(Qt.AlignCenter) 
+        self.lEditSensorId.setAlignment(Qt.AlignCenter) 
+
+
+    def loadIcons(self):
+
+        self.lblQcStatusIcon.setPixmap(QPixmap("Icons/status-offline.png"))
+        self.lblQcStatusIcon.setScaledContents(True)
+        self.lblClassResultIcon.setPixmap(QPixmap("Icons/status-offline.png"))
+        self.lblClassResultIcon.setScaledContents(True) 
+
+
     def initUI(self):
 
         uic.loadUi("C:/Users/TeraSmart/Documents/API_MenloSystem/RamGlobalSystem/TQC/View/Designer/MainWindow.ui", self)
         self.setWindowTitle("THEA QC v1.05")
+        self.disableButtons()
+        self.loadIcons()
         self.livePlot.setLabel('left', 'Transmission Intensity (dB)')
         self.livePlot.setLabel('bottom', 'Frequency (THz)')
         self.livePlot.setTitle("""Live QC""", color = 'g', size = "45 pt")   
@@ -36,11 +80,54 @@ class TqcMainWindow(QWidget):
         self.livePlot.setMouseTracking(True)
         
         self.textbox.setReadOnly(True)
-        # self.progAvg.setValue(self.avgProgVal)
-        # self.progTimelapse.setValue(self.tlapseProgVal)
+        self.progAvg.setValue(self.experiment.avgProgVal)
+        self.progTimelapse.setValue(self.experiment.tlapseProgVal)
+
+
+    def disableButtons(self):
+
+        """
+            Disable GUI buttons from user interactions.
+        """
+
+        self.btnStart.setEnabled(False)
+        self.btnResetAvg.setEnabled(False)
+        self.btnStartAveraging.setEnabled(False)
+        self.btnStartTimelapse.setEnabled(False)
+        self.btnStartQC.setEnabled(False)
+        self.btnFinishQC.setEnabled(False)
+        self.btnNewStdRef.setEnabled(False)
+
+
+    def enableButtons(self):
+
+        """
+            Enable GUI buttons for user interaction
+        """
+
+        self.btnStartTimelapse.setEnabled(True)
+        self.btnStart.setEnabled(True)
+        self.btnStop.setEnabled(True)
+        self.btnStartQC.setEnabled(True)
+        self.btnStartAveraging.setEnabled(True)
+        self.btnResetAvg.setEnabled(True)
+
+
+    def message(self, msgStrPattern):
+
+        """
+            Prints messages in the GUI Output textbox
+        """
+
+        self.textbox.appendPlainText(f"{msgStrPattern}")
 
 
 
+
+
+
+
+#******************************************************************************************
 if __name__ == '__main__':
     
     
@@ -49,7 +136,7 @@ if __name__ == '__main__':
     asyncio.set_event_loop(loop)
 
     tqc = TheaQC(loop, '../Model/theaConfig.yml')
-    win = TqcMainWindow()
+    win = TqcMainWindow(tqc)
     win.show()
 
     with loop:
