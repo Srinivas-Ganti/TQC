801f08251ece22e29012cf5a72ab2f4268b05dbf
Srinivas Ganti RGLab <srinivas.ganti@ramglobal.com>
2021-11-23 00:43:54 +0100

    add validators, button behaviours

--- View/TqcMainWindow.py
+++ View/TqcMainWindow.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 
@@ -14,9 +13,9 @@ sys.path.append(os.path.join(viewDir, "Icons"))
 sys.path.append(uiDir)
 
 from Model.TheaQC import *
+from Model import ur
 
-
-
+from pint.errors import *
 
 class TqcMainWindow(QWidget):
 
@@ -27,6 +26,260 @@ class TqcMainWindow(QWidget):
         self.initUI()
 
 
+    def connectEvents(self):
+        
+        """
+            Connect GUI object and validator signals to respective methods.
+        """
+        
+        self.btnStart.clicked.connect(self.experiment.device.start)
+        self.btnStop.clicked.connect(self.experiment.device.stop) 
+        # self.livePlot.scene().sigMouseMoved.connect(self.mouseMoved)
+        # self.btnResetAvg.clicked.connect(self.experiment.device.resetAveraging)
+        # self.btnStartAveraging.clicked.connect(self.startAveraging)
+        # self.btnStartTimelapse.clicked.connect(self.startTimelapse)
+        # self.btnStartQC.clicked.connect(self.startQC)
+        # self.btnFinishQC.clicked.connect(self.finishQC)
+        # self.btnNewStdRef.clicked.connect(self.measureStandardRef)
+        # self.dataUpdateReady.connect(self.updatePlot)
+        # self.sensorUpdateReady.connect(self.checkNextSensor)
+
+        self.lEditTdsStart.editingFinished.connect(self.validateEditStart)
+        self.lEditTdsEnd.setReadOnly(True)
+        self.lEditTdsAvgs.editingFinished.connect(self.validateEditAverages)
+        self.lEditInterval.editingFinished.connect(self.validateEditInterval)
+        self.lEditRepeatNum.editingFinished.connect(self.validateEditRepeats) 
+        self.lEditWaferId.editingFinished.connect(self.validateEditWaferId)
+        self.lEditSensorId.editingFinished.connect(self.validateEditSensorId)
+        
+
+    def validateEditStart(self):
+
+        """
+            Validate user input for Start time
+        """
+
+        validationRule = QDoubleValidator(-420,420,3)
+        print(validationRule.validate(self.lEditTdsStart.text(),420))
+        if validationRule.validate(self.lEditTdsStart.text(),
+                                   420)[0] == QValidator.Acceptable:
+            print("TDS START TIME ACCEPTED")
+            self.experiment.startOk = True
+            self.experiment.startTime = float(self.lEditTdsStart.text())
+            
+            self.experiment.endTime = float(self.lEditTdsStart.text()) + self.experiment.TdsWin
+            self.lEditTdsEnd.setText(str(self.experiment.endTime))
+            self.experiment.endOk = True
+            self.experiment.tdsParams['start'] = self.experiment.startTime
+            self.experiment.tdsParams['end'] = self.experiment.endTime
+            print(f"End time is set to be {self.experiment.TdsWin} ps after THz pulse start at {self.experiment.startTime} ps")
+            print("TDS END TIME ACCEPTED")
+        else:
+            print("[ERROR] InvalidInput: Setting default config value")
+            self.experiment.startOk = False
+            self.lEditTdsStart.setText(str(self.experiment.config['TScan']['begin']))
+
+
+    def validateDefaultInputs(self):
+
+        self.lEditTdsStart.setText(str(self.experiment.config['TScan']['begin']))
+        self.lEditTdsStart.editingFinished.emit()
+        self.lEditTdsAvgs.setText(str(self.experiment.config['TScan']['numAvgs']))
+        self.lEditTdsAvgs.editingFinished.emit()
+        self.loadTDSParams()
+        print("> [SCANCONTROL] Setting TDS parameters: Done\n")
+                 
+        self.lEditInterval.setText(str(self.experiment.config['Timelapse']['interval']))
+        self.lEditInterval.editingFinished.emit()
+        self.lEditRepeatNum.setText(str(self.experiment.config['Timelapse']['repeats']))
+        self.lEditRepeatNum.editingFinished.emit()
+        self.loadTimelapseParams()
+        print("> [TIMELAPSE] Setting Timelapse parameters: Done\n")
+        self.chipsPerWafer = int(self.experiment.config['QC']['chipsPerWafer'])
+        self.lEditWaferId.setText(str(self.experiment.config['QC']['waferId']))
+        self.lEditWaferId.editingFinished.emit()
+        self.lEditSensorId.setText(str(self.experiment.config['QC']['sensorId']))
+        self.lEditSensorId.editingFinished.emit() 
+
+        self.loadQC()
+
+
+    def loadTDSParams(self):
+
+        """
+            Load the TDS measurement parameters related to pulse duration and timing, required number of averages from config file.
+        """
+
+        if (self.experiment.startOk and self.experiment.endOk and self.experiment.avgsOk):
+            print("TDS Inputs accepted")
+            self.experiment.device.setBegin(self.experiment.startTime)
+            self.experiment.device.setEnd(self.experiment.endTime)
+            self.experiment.device.setDesiredAverages(self.experiment.numAvgs)
+            self.btnStart.setEnabled(True)
+            self.btnStartAveraging.setEnabled(True)
+            self.btnResetAvg.setEnabled(True)
+
+
+    def loadQC(self):
+
+        """
+            Load QC parameters from config file and validate inputs
+        """
+
+        self.experiment.loadQcConfig()
+        if self.experiment.waferIdOk and self.experiment.sensorIdOk and self.experiment.keepRunning:
+            self.btnStartQC.setEnabled(True)
+        else:
+            self.btnStartQC.setEnabled(False)
+    
+
+    def loadTimelapseParams(self):
+
+        """
+            Load timelapse measurement parameters related to interval and duration.
+        """
+
+        if self.experiment.intervalOK and self.experiment.repeatsOk:
+            self.btnStartTimelapse.setEnabled(True)
+            self.lblTlapseStatus.setText("Timelapse: Ready")
+        else:
+            self.btnStartTimelapse.setEnabled(False)
+            self.lblTlapseStatus.setText("Timelapse: Not available")
+
+
+    
+    def validateEditAverages(self):
+
+        """
+            Validate user input for required number of averages.
+        """
+                
+        validationRule = QIntValidator(1,2000)
+        print(validationRule.validate(self.lEditTdsAvgs.text(),2000))
+        if validationRule.validate(self.lEditTdsAvgs.text(),
+                                   2000)[0] == QValidator.Acceptable:
+            print("TDS AVGS ACCEPTED")
+            self.experiment.avgsOk = True
+            self.experiment.numAvgs = int(self.lEditTdsAvgs.text())
+            self.experiment.tdsParams['numAvgs'] = self.experiment.numAvgs
+        else:
+            print("[ERROR] InvalidInput: Setting default config value")
+            self.experiment.avgsOK = False
+            self.lEditTdsAvgs.setText(str(self.experiment.config['TScan']['numAvgs']))
+
+        
+    def validateEditInterval(self):
+        
+        """
+            Validate user input for timelapse interval - check units for consistency
+        """
+
+        try:
+            self.experiment.intervalOk = False
+            isinstance(ur(self.lEditInterval.text()), ur.Quantity)
+            interval = ur(self.lEditInterval.text())
+            if isinstance(interval, int):
+                interval = interval*ur("s")
+
+            if interval.units in ["second", "minute", "hour"]: 
+                print("Interval is a valid time input")
+                print("Timelapse interval ACCEPTED")
+                self.experiment.interval = interval.m_as("second")
+                print(f"Interval is set to {self.interval} seconds")
+            else:
+                print("Interval units are not valid time inputs, Setting default config values")
+                print("Enter valid units, for example: '0.5hour', '420ms', '5min' . . . ")
+                self.lEditInterval.setText(str(self.experiment.config['Timelapse']['interval']))
+                self.interval = ur(str(self.experiment.config['Timelapse']['interval'])).m_as("second")
+            self.experiment.intervalOK = True
+        except UndefinedUnitError:
+            self.experiment.intervalOK = False
+            print("Undefined / Incorrect units. Setting default config value")
+            self.lEditInterval.setText(str(self.experiment.config['Timelapse']['interval']))
+            self.experiment.interval = ur(str(self.experiment.config['Timelapse']['interval']))
+            self.experiment.intervalOK = True
+            
+    
+    def validateEditRepeats(self):
+        
+        """
+            Validate user input for timelapse repeats - check units and dimensions for consistency
+        """
+
+        self.experiment.repeatsOk = False
+        validationRule = QIntValidator(1,2000)
+            
+        print(validationRule.validate(self.lEditRepeatNum.text(),2000))
+        if validationRule.validate(self.lEditRepeatNum.text(),
+                                   2000)[0] == QValidator.Acceptable:
+            print("Timelapse frames count ACCEPTED")
+            self.experiment.repeatNum = int(self.lEditRepeatNum.text())
+            
+        else:
+            print("Repeats must be non zero integers")
+            
+            self.experiment.repeatNum = int(self.experiment.config['Timelapse']['repeats'])
+            print(f"Setting default config value: {self.experiment.repeatNum} frames")
+            self.lEditRepeatNum.setText(str(self.experiment.repeatNum))
+        self.experiment.repeatsOk = True
+
+
+    def validateEditWaferId(self):
+
+        """
+            Validate user input for QC wafer ID - check naming format for consistency
+        """
+
+        self.experiment.waferIdOk = False
+        wId = self.lEditWaferId.text()
+        
+        if wId.startswith("W"):
+            try:    
+                wnum = int(wId.split("W")[1])
+                print(f"Wafer ID ACCEPTED: W{wnum}")
+                self.experiment.waferId = f"W{wnum}"
+            except ValueError:
+                print("Wafer ID must follow the pattern 'W<integer>'")
+                print("Setting default config value")
+                self.lEditWaferId.setText(self.experiment.config['QC']['waferId'])
+                self.experiment.waferId = self.experiment.config['QC']['waferId']
+        else:
+            print("Wafer ID must follow the pattern 'W<integer>'")
+            print("Setting default config value")
+            self.lEditWaferId.setText(self.experiment.config['QC']['waferId'])
+            self.experiment.waferId = self.experiment.config['QC']['waferId']
+        self.experiment.waferIdOk = True
+        if not self.experiment.waferIdOk:
+            self.btnStartQC.setEnabled(False)
+        
+
+    def validateEditSensorId(self):
+                
+        """
+            Validate user input for QC/timelapse sensor ID.
+        """
+
+        
+        self.experiment.sensorIdOk = False
+        validationRule = QIntValidator(1,self.experiment.chipsPerWafer)                         
+        print(validationRule.validate(self.lEditSensorId.text(),self.experiment.chipsPerWafer))
+        if validationRule.validate(self.lEditSensorId.text(),
+                                  self.chipsPerWafer)[0] == QValidator.Acceptable:
+            print("Sensor ID ACCEPTED")
+            self.experiment.sensorId =  int(self.lEditSensorId.text())
+        else:
+            print(f"Sensor ID must be non zero integer in range of (1 , {self.experiment.chipsPerWafer})")
+            
+            self.experiment.sensorId = int(self.experiment.config['QC']['sensorId'])
+            print(f"Setting default config value: Sensor ID is set to {self.experiment.sensorId}")
+            self.lEditSensorId.setText(str(self.experiment.sensorId))
+        self.experiment.sensorIdOk = True
+        if not self.experiment.sensorIdOk:
+            self.btnStartQC.setEnabled(False)
+
+#######
+
+
     def initAttribs(self):
 
         """
@@ -34,7 +287,7 @@ class TqcMainWindow(QWidget):
         """
 
         self.height = 900
-        self.width = 720
+        self.width = 750
         self.left = 10
         self.top = 40      
         self.setGeometry(self.left, self.top, self.width, self.height)   
@@ -69,10 +322,14 @@ class TqcMainWindow(QWidget):
 
     def initUI(self):
 
-        uic.loadUi("C:/Users/TeraSmart/Documents/API_MenloSystem/RamGlobalSystem/TQC/View/Designer/MainWindow.ui", self)
+        uic.loadUi("../View/Designer/MainWindow.ui", self)
         self.setWindowTitle("THEA QC v1.05")
         self.disableButtons()
         self.loadIcons()
+        self.initAttribs()
+        self.connectEvents()
+        self.validateDefaultInputs()
+
         self.livePlot.setLabel('left', 'Transmission Intensity (dB)')
         self.livePlot.setLabel('bottom', 'Frequency (THz)')
         self.livePlot.setTitle("""Live QC""", color = 'g', size = "45 pt")   
